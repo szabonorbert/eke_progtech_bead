f8545a671b92aa7002af90f3597eb5c4c2f2e979
unknown <szabonorbert@detenyleg.com>
2020-04-25 17:50:55 +0200

    Add setters, getters, ToString and equals to Car

--- src/main/java/hu/szabonorbert/eke_progtech/Car.java
+++ src/main/java/hu/szabonorbert/eke_progtech/Car.java
@@ -1,6 +1,6 @@
 package hu.szabonorbert.eke_progtech;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class Car {
     private String manufacturer;
@@ -13,5 +13,50 @@ public class Car {
         this.type = type;
         this.color = color;
         this.condition = condition;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getCondition() {
+        return condition;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCondition(double condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(car.condition, condition) == 0 &&
+                Objects.equals(manufacturer, car.manufacturer) &&
+                Objects.equals(type, car.type) &&
+                Objects.equals(color, car.color);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "manufacturer='" + manufacturer + '\'' +
+                ", type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                ", condition=" + condition +
+                '}';
     }
 }
