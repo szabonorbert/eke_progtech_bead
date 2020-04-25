7c05e3eb1ea1108f2626697c215b2d738161803d
unknown <szabonorbert@detenyleg.com>
2020-04-25 17:46:45 +0200

    Car basics

--- src/main/java/hu/szabonorbert/eke_progtech/Car.java
+++ src/main/java/hu/szabonorbert/eke_progtech/Car.java
@@ -1,5 +1,17 @@
 package hu.szabonorbert.eke_progtech;
 
-public class Car {
+import java.util.ArrayList;
 
+public class Car {
+    private String manufacturer;
+    private String type;
+    private String color;
+    private double condition;
+
+    public Car(String manufacturer, String type, String color, double condition) {
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.color = color;
+        this.condition = condition;
+    }
 }
