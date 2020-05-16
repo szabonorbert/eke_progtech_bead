6f840b5145d62e956cf11ed8828abb8f5c3079b6
Norbert <szabonorbert@detenyleg.com>
2020-05-16 12:23:30 +0200

    The manager is singleton now!

--- src/main/java/hu/szabonorbert/eke_progtech/App.java
+++ src/main/java/hu/szabonorbert/eke_progtech/App.java
@@ -2,9 +2,12 @@ package hu.szabonorbert.eke_progtech;
 
 public class App {
     public static void main(String args[]) {
-        Person jozsi = new Manager("Józsi");
+
         Person csabi = new Customer("Csabi");
-        jozsi.Speak("Üdv nálunk!");
         csabi.Speak("Jó napot!");
+
+        Person jozsi = Manager.getInstance();
+        jozsi.Speak("Üdv nálunk!");
+
     }
 }
--- src/main/java/hu/szabonorbert/eke_progtech/Manager.java
+++ src/main/java/hu/szabonorbert/eke_progtech/Manager.java
@@ -1,9 +1,19 @@
 package hu.szabonorbert.eke_progtech;
 
 public class Manager extends Person {
-    public Manager(String name) {
+
+    private static Manager uniqueInstance = null;
+
+    private Manager(String name) {
         super(name);
     }
+
+    public static Manager getInstance(){
+        if (uniqueInstance == null) uniqueInstance = new Manager("Józsi");
+        return uniqueInstance;
+    }
+
+
     public void Speak(String sentence){
         System.out.println(name + " (menedzser): - " + sentence);
     }
