6f386b890ae2b6e1d0f934cce807da2b63f9c248
Norbert <szabonorbert@detenyleg.com>
2020-05-16 11:59:23 +0200

    Car update year attribute instead of color

--- src/main/java/hu/szabonorbert/eke_progtech/Car.java
+++ src/main/java/hu/szabonorbert/eke_progtech/Car.java
@@ -5,34 +5,25 @@ import java.util.Objects;
 public class Car {
     private String manufacturer;
     private String type;
-    private String color;
+    private int year;
     private double condition;
 
-    public Car(String manufacturer, String type, String color, double condition) {
+    public Car(String manufacturer, String type, int year, double condition) {
         this.manufacturer = manufacturer;
         this.type = type;
-        this.color = color;
+        this.year = year;
         this.condition = condition;
     }
 
     public String getManufacturer() {
         return manufacturer;
     }
-
-    public String getType() {
-        return type;
+    public String getType() { return type; }
+    public int getYear() {
+        return year;
     }
-
-    public String getColor() {
-        return color;
-    }
-
     public double getCondition() {
         return condition;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public void setCondition(double condition) {
@@ -47,7 +38,7 @@ public class Car {
         return Double.compare(car.condition, condition) == 0 &&
                 Objects.equals(manufacturer, car.manufacturer) &&
                 Objects.equals(type, car.type) &&
-                Objects.equals(color, car.color);
+                Objects.equals(year, car.year);
     }
 
     @Override
@@ -55,7 +46,7 @@ public class Car {
         return "Car{" +
                 "manufacturer='" + manufacturer + '\'' +
                 ", type='" + type + '\'' +
-                ", color='" + color + '\'' +
+                ", year='" + year + '\'' +
                 ", condition=" + condition +
                 '}';
     }
